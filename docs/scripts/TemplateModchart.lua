8af889d3162565648e14a965c64ecd7700ff9864
Mily_0 <135561818+0mily@users.noreply.github.com>
2026-04-16 01:28:08 -0300

    por agora vai ficar assim fdssss
    lrgal

---
+++ docs/scripts/TemplateModchart.lua
@@ -0,0 +1,44 @@
+function modChartCreate()
+end
+
+function modChartCreatePost()
+end
+
+function modChartUpdate(elapsed)
+end
+
+function modChartUpdatePost(elapsed)
+end
+
+function modChartStepHit()
+end
+
+function modChartBeatHit()
+end
+
+function modChartSectionHit()
+end
+
+function modChartFocus(focus)
+end
+
+function modChartBFNote(id, nd, nt, sus)
+end
+
+function modChartDADNote(id, nd, nt, sus)
+end
+
+function modChartBFNotePre(id, nd, nt, sus)
+end
+
+function modChartDADNotePre(id, nd, nt, sus)
+end
+
+function modChartMiss(id, nd, nt, sus)
+end
+
+function modChartMissPress(id, nd, nt, sus)
+end
+
+function modChartSongStart()
+end
